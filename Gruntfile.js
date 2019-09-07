5c805724ae2d1e99ac1008e4aab08035cfa4cf7a
Peter Newman <peternewman@users.noreply.github.com>
2019-09-07 12:18:45 +0100

    Also run test Draft 6 and 7 test suite tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,6 +36,30 @@ module.exports = function Gruntfile(grunt) {
           },
         ],
       },
+      draft6: {
+        files: [
+          {
+            expand: true,
+            src: jsonSchemaTestSuite + '/draft6/*.json',
+          },
+          {
+            expand: true,
+            src: jsonSchemaTestSuite + '/draft6/optional/*.json',
+          },
+        ],
+      },
+      draft7: {
+        files: [
+          {
+            expand: true,
+            src: jsonSchemaTestSuite + '/draft7/*.json',
+          },
+          {
+            expand: true,
+            src: jsonSchemaTestSuite + '/draft7/optional/*.json',
+          },
+        ],
+      },
     },
   });
 
